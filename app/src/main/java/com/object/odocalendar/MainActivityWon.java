f661eb0f619dc0bd71fa597335e43dba6e2437e8
wonkgb0922 <wonkgb0922@gmail.com>
2023-12-02 21:10:25 +0900

    12/02

--- app/src/main/java/com/object/odocalendar/MainActivityWon.java
+++ app/src/main/java/com/object/odocalendar/MainActivityWon.java
@@ -4,17 +4,17 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.view.WindowManager;
 import android.widget.CalendarView;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivityWon extends AppCompatActivity {
 
     private TodoPopupActivity act;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_won);
 
         WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
         layoutParams.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
@@ -22,16 +22,19 @@ public class MainActivity extends AppCompatActivity {
         layoutParams.dimAmount = 0.8f;
         getWindow().setAttributes(layoutParams);
 
+        ListView lv = findViewById(R.id.TodoListView);
         cv.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
-            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
-                Popup();
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
+                // 년, 월, 일을 파라미터로 Popup에 전달
+                Popup(year, month, day);
             }
         });
     }
 
-    public void Popup() {
-        act = new TodoPopupActivity(this);
+    public void Popup(int year, int month, int day) {
+        // 년, 월, 일을 파라미터로 팝업 액티비티 생성
+        act = new TodoPopupActivity(this, year, month, day);
         act.show();
     }
 }
